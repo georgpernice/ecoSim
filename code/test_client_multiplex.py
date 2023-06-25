3b58ef158f0eb5764474d1d92dcd0146af0055e5
georg <georgpernice@gmail.com>
2023-06-25 16:29:39 +0200

    This state is quite errorfree. Multiplex server displaying messages of multiple clients. TBD: Server answers to clients, Server accesses database while tlking with the clients. Second server that displays GUI in a html port

--- code/test_client_multiplex.py
+++ code/test_client_multiplex.py
@@ -1,6 +1,10 @@
 import socket
-print("Starting a client. Enter your desired connection.. ")
+
+# user print context & obtain IPv4 address
+print("Starting multiplex client. Enter your desired connection.. ")
 ip = input("IP-Adresse: ")
+
+# initialize a socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, 50000))
 try:
